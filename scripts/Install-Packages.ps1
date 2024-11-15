67487f31c3ba4815da7c3b4c72f3b6637f48d3b7
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2024-11-15 14:51:03 -0800

    feat: enhance package installation with better error handling and upgrade checks

--- scripts/Install-Packages.ps1
+++ scripts/Install-Packages.ps1
@@ -1,120 +1,171 @@
 function Install-Packages {
+  $packageCategories = @("compatibility", "drivers", "utilities", "development", "design-video", "communication-multimedia", "productivity", "gaming")
   $packages = @{
-    "compatibility" = @(
+    "compatibility"            = @(
       "EclipseAdoptium.Temurin.17.JRE",
       "EclipseAdoptium.Temurin.21.JRE",
       "EclipseAdoptium.Temurin.8.JRE",
+      "Git.Git",
       "Microsoft.DirectX",
-      "Microsoft.DotNet.DesktopRuntime.7",
-      "Microsoft.DotNet.DesktopRuntime.8",
       "Microsoft.PowerShell",
-      "Microsoft.PowerToys",
-      "Microsoft.WindowsTerminal",
       "Microsoft.XNARedist",
       "OpenAL.OpenAL"
     )
-    "tools"         = @(
+    "drivers"                  = @(
+      "Logitech.GHUB",
+      "seerge.g-helper",
+      "Wagnardsoft.DisplayDriverUninstaller"
+    )
+    "utilities"                = @(
       "7zip.7zip",
       "9P7KNL5RWT25", # SysInternals Suite
+      "Bitwarden.Bitwarden",
       "BleachBit.BleachBit",
-      "Git.Git",
       "IDRIX.VeraCrypt",
       "LocalSend.LocalSend",
       "MHNexus.HxD",
+      "Microsoft.PowerToys",
+      "Proton.ProtonVPN",
       "PuTTY.PuTTY",
+      "RevoUninstaller.RevoUninstaller",
       "Safing.Portmaster",
-      "TechPowerUp.NVCleanstall",
       "voidtools.Everything",
-      "Wagnardsoft.DisplayDriverUninstaller",
       "winaero.tweaker",
       "WinDirStat.WinDirStat",
       "WinSCP.WinSCP",
       "yt-dlp.yt-dlp"
     )
-    "apps"          = @(
-      "9NKSQGP7F2NH", # WhatsApp
-      "Anki.Anki",
-      "AnyAssociation.Anytype",
-      "Audacity.Audacity",
-      "Bitwarden.Bitwarden",
-      "Discord.Discord",
+    "development"              = @(
       "Docker.DockerDesktop",
+      "Google.AndroidStudio",
+      "Microsoft.VisualStudioCode"
+    )
+    "design-video"             = @(
       "dotPDN.PaintDotNet",
       "Figma.Figma",
-      "Google.AndroidStudio",
+      "OBSProject.OBSStudio"
+    )
+    "communication-multimedia" = @(
+      "9NKSQGP7F2NH", # WhatsApp
+      "Audacity.Audacity",
+      "Discord.Discord",
+      "Spotify.Spotify"
+    )
+    "productivity"             = @(
+      "Anki.Anki",
+      "AnyAssociation.Anytype"
+    )
+    "gaming"                   = @(
       "HeroicGamesLauncher.HeroicGamesLauncher",
-      "LibreWolf.LibreWolf",
-      "Logitech.GHUB",
-      "Microsoft.VisualStudioCode",
-      "OBSProject.OBSStudio",
-      "Proton.ProtonVPN",
-      "RevoUninstaller.RevoUninstaller",
-      "seerge.g-helper",
-      "Spotify.Spotify",
+      "LizardByte.Sunshine",
+      "PrismLauncher.PrismLauncher",
       "Valve.Steam"
     )
   }
 
-  $confirm = Read-Host "Do you want to install packages? (Y/N)"
-  if ($confirm -ne 'Y') {
-    Write-Host "Skipping package installation." -ForegroundColor Yellow
-    return
-  }
+  $failedPackages = @()
 
   # Download and install Visual C++ Redistributable AIO package
   $vcRedistUrl = "https://github.com/abbodi1406/vcredist/releases/latest/download/VisualCppRedist_AIO_x86_x64.exe"
   $vcRedistPath = Join-Path $env:TEMP "VisualCppRedist_AIO_x86_x64.exe"
 
-  Write-Host "Downloading Visual C++ Redistributable AIO package..." -ForegroundColor Cyan
-  try {
-    Invoke-WebRequest -Uri $vcRedistUrl -OutFile $vcRedistPath
-    Write-Host "Download completed." -ForegroundColor Green
+  $confirmVCRedist = Read-Host "Do you want to install Visual C++ Redistributable AIO package? (Y/N)"
+  if ($confirmVCRedist -eq 'Y') {
+    Write-Host "Downloading Visual C++ Redistributable AIO package..." -ForegroundColor Cyan
+    try {
+      Invoke-WebRequest -Uri $vcRedistUrl -OutFile $vcRedistPath
+      Write-Host "Download completed." -ForegroundColor Green
 
-    Write-Host "Installing Visual C++ Redistributable AIO package..." -ForegroundColor Cyan
-    Start-Process -FilePath $vcRedistPath -ArgumentList "/ai /gm2" -Wait -NoNewWindow
-    Write-Host "Installation completed." -ForegroundColor Green
+      Write-Host "Installing Visual C++ Redistributable AIO package..." -ForegroundColor Cyan
+      Start-Process -FilePath $vcRedistPath -ArgumentList "/ai /gm2" -Wait -NoNewWindow
+      Write-Host "Installation completed." -ForegroundColor Green
 
-    # Clean up the downloaded file
-    Remove-Item -Path $vcRedistPath -Force
+      # Clean up the downloaded file
+      Remove-Item -Path $vcRedistPath -Force
+    }
+    catch {
+      Write-Host "Error downloading or installing Visual C++ Redistributable AIO package." -ForegroundColor Red
+      Write-Host $_.Exception.Message
+    }
   }
-  catch {
-    Write-Host "Error downloading or installing Visual C++ Redistributable AIO package." -ForegroundColor Red
-    Write-Host $_.Exception.Message
+  else {
+    Write-Host "Skipping Visual C++ Redistributable AIO package installation." -ForegroundColor Yellow
   }
 
-  $totalPackages = ($packages.Values | ForEach-Object { $_.Count } | Measure-Object -Sum).Sum
+  $totalPackages = 0
   $progress = 0
 
-  # Install winget packages
-  foreach ($category in $packages.Keys | Sort-Object) {
-    Write-Host "Installing $category packages..." -ForegroundColor Cyan
-    foreach ($package in $packages[$category]) {
-      $progress++
-      $status = "Installing package: $package"
-      $percentComplete = [math]::Min(($progress / $totalPackages) * 100, 100)
-      Write-Progress -Activity "Installing Packages" -Status $status -PercentComplete $percentComplete
-
-      try {
-        $installed = winget list --id $package --accept-source-agreements | Select-String -Pattern "^$package"
-        if ($installed) {
-          Write-Host "Package already installed: $package" -ForegroundColor Yellow
+  function Install-Package($package) {
+    try {
+      $listOutput = winget list --id $package --accept-source-agreements 2>&1
+      $installed = $listOutput | Select-String -Pattern "^$package"
+      
+      if ($installed) {
+        # Check if an upgrade is available
+        $upgradeOutput = winget upgrade --id $package --accept-source-agreements 2>&1
+        if ($upgradeOutput -match "No available upgrade found.") {
+          Write-Host "Package already installed and up to date: $package" -ForegroundColor Green
         }
         else {
-          winget install --id $package --accept-package-agreements --accept-source-agreements
-          if ($LASTEXITCODE -eq 0) {
-            Write-Host "Successfully installed: $package" -ForegroundColor Green
-          }
-          else {
-            Write-Host "Failed to install: $package" -ForegroundColor Red
-          }
+          Write-Host "Upgrading package: $package" -ForegroundColor Cyan
+          winget upgrade --id $package --silent --accept-source-agreements --accept-package-agreements
+          Write-Host "Successfully upgraded: $package" -ForegroundColor Green
+        }
+        return $true
+      }
+      else {
+        Write-Host "Installing package: $package" -ForegroundColor Cyan
+        $installOutput = winget install --id $package --silent --accept-package-agreements --accept-source-agreements 2>&1
+        if ($LASTEXITCODE -eq 0) {
+          Write-Host "Successfully installed: $package" -ForegroundColor Green
+          return $true
+        }
+        else {
+          Write-Host "Failed to install: $package" -ForegroundColor Red
+          Write-Host $installOutput
+          return $false
         }
       }
-      catch {
-        Write-Host "Error installing package: $package" -ForegroundColor Red
-        Write-Host $_.Exception.Message
-      }
+    }
+    catch {
+      Write-Host "Error processing package: $package" -ForegroundColor Red
+      Write-Host $_.Exception.Message
+      return $false
     }
   }
 
-  Write-Progress -Activity "Installing Packages" -Completed
+  # Install winget packages
+  foreach ($category in $packageCategories) {
+    $confirmCategory = Read-Host "Do you want to install $category packages? (Y/N)"
+    if ($confirmCategory -eq 'Y') {
+      $totalPackages += $packages[$category].Count
+      Write-Host "Processing $category packages..." -ForegroundColor Cyan
+      foreach ($package in $packages[$category]) {
+        $progress++
+        $status = "Processing package: $package"
+        $percentComplete = [math]::Min(($progress / $totalPackages) * 100, 100)
+        Write-Progress -Activity "Processing Packages" -Status $status -PercentComplete $percentComplete
+
+        if (-not (Install-Package $package)) {
+          $failedPackages += $package
+        }
+      }
+    }
+    else {
+      Write-Host "Skipping $category packages." -ForegroundColor Yellow
+    }
+  }
+
+  Write-Progress -Activity "Processing Packages" -Completed
+
+  # Report on failed packages
+  if ($failedPackages.Count -gt 0) {
+    Write-Host "The following packages failed to install:" -ForegroundColor Red
+    foreach ($package in $failedPackages) {
+      Write-Host "  - $package" -ForegroundColor Red
+    }
+  }
+  else {
+    Write-Host "All packages were processed successfully." -ForegroundColor Green
+  }
 }
